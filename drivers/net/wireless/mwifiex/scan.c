5f2b951d9288882f76c34a4f0e7ab8eef53cd3c6
Carlo Caione <carlo@endlessm.com>
2018-07-17 11:53:08 +0100

    Partially revert [nl80211: support beacon report scanning]
    SHA: 1d76250bd34a
    
    Signed-off-by: Carlo Caione <carlo@endlessm.com>

--- drivers/net/wireless/mwifiex/scan.c
+++ drivers/net/wireless/mwifiex/scan.c
@@ -1987,13 +1987,9 @@ static void mwifiex_check_next_scan_command(struct mwifiex_private *priv)
 			mwifiex_complete_scan(priv);
 
 		if (priv->scan_request) {
-			struct cfg80211_scan_info info = {
-				.aborted = false,
-			};
-
 			mwifiex_dbg(adapter, INFO,
 				    "info: notifying scan done\n");
-			cfg80211_scan_done(priv->scan_request, &info);
+			cfg80211_scan_done(priv->scan_request, 0);
 			priv->scan_request = NULL;
 			priv->scan_aborting = false;
 		} else {
@@ -2013,13 +2009,9 @@ static void mwifiex_check_next_scan_command(struct mwifiex_private *priv)
 
 		if (!adapter->active_scan_triggered) {
 			if (priv->scan_request) {
-				struct cfg80211_scan_info info = {
-					.aborted = true,
-				};
-
 				mwifiex_dbg(adapter, INFO,
 					    "info: aborting scan\n");
-				cfg80211_scan_done(priv->scan_request, &info);
+				cfg80211_scan_done(priv->scan_request, 1);
 				priv->scan_request = NULL;
 				priv->scan_aborting = false;
 			} else {
@@ -2059,13 +2051,9 @@ void mwifiex_cancel_scan(struct mwifiex_adapter *adapter)
 			if (!priv)
 				continue;
 			if (priv->scan_request) {
-				struct cfg80211_scan_info info = {
-					.aborted = true,
-				};
-
 				mwifiex_dbg(adapter, INFO,
 					    "info: aborting scan\n");
-				cfg80211_scan_done(priv->scan_request, &info);
+				cfg80211_scan_done(priv->scan_request, 1);
 				priv->scan_request = NULL;
 				priv->scan_aborting = false;
 			}
